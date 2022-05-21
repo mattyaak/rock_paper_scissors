fcb76cd180053b114a9542d56f1814b528f0bb83
Matty <to.akos@gmail.com>
2022-05-21 09:30:41 +0200

    final js

--- script.js
+++ script.js
@@ -3,12 +3,46 @@ console.log("Hello World!")
 function computerplay () {
     let a = Math.random()
     if (a < 0.33) {
-        console.log("rock")
+        return "rock"
     } else if (a>=0.33 && a <0.66) {
-        console.log("paper")
+        return "paper"
     } else {
-        console.log ("scissors")
+        return "scissors"
     }
 }
 
-computerplay()
+let a = computerplay()
+
+const input = prompt("Rock or Paper or Scissors?");
+
+
+if (a == "rock" && input == "paper" ){
+    console.log(a)
+    console.log("You win!")
+} else if (a == "rock" && input =="rock" ){
+    console.log(a)
+    console.log("Both showed rock!") 
+} else if (a == "rock" && input == "scissors" ){
+    console.log(a)
+    console.log("You lost") 
+} else if (a == "paper" && input == "paper" ){
+    console.log(a)
+    console.log("Both showed paper!") 
+} else if (a == "paper" && input == "rock" ){
+    console.log(a)
+    console.log("You lost!") 
+} else if (a == "paper" && input == "scissors" ){
+    console.log(a)
+    console.log("You win!") 
+} else if (a  == "scissors" && input =="paper" ){
+    console.log(a)
+    console.log("You lost!") 
+} else if (a == "scissors" && input =="scissors" ){
+    console.log(a)
+    console.log("Both showed scissors!") 
+} else if (a == "scissors" && input =="rock" ){
+    console.log(a)
+    console.log("You win!") 
+} else {
+    console.log("game over!")
+ }
